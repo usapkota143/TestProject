fa9bc938d5973fa4ee65eb3af664c5a487f4548d
Umesh Sapkota <usapkota143@gmail.com>
2022-02-14 18:09:49 -0700

    added some code

--- Testproject/src/com/controller/MainController.java
+++ Testproject/src/com/controller/MainController.java
@@ -1,5 +1,13 @@
 package com.controller;
 
 public class MainController {
+	
+	private void sysout() {
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		System.out.println("hello budi");
+	}
 
 }
